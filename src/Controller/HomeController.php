9007defeb9c9a8e137b54c0f348c07e3deecc398
Baptiste Rossignol <baptiste.rossignol@hotmail.fr>
2019-01-27 14:01:53 +0100

    3/16 - Découverte de doctrine

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Twig\Environment;
+use App\Repository\PropertyRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,8 +14,10 @@ class HomeController extends AbstractController
      * @Route("/", name="home")
      * @return Response
      */
-    public function index(): Response
+    public function index(PropertyRepository $repository): Response
     {
-        return $this->render('pages/home.html.twig');
+        $properties = $repository->findLatest();
+        return $this->render('pages/home.html.twig', [
+        'properties' => $properties]);
     }
 }
